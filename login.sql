996cd0a6ea37b16ee9bfcdc604584f8b2e59b771
dddextermorgan8-design <dddextermorgan8@gmail.com>
2026-03-18 12:51:25 -0300

    Update login.sql

--- login.sql
+++ login.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 18-03-2026 a las 16:44:25
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -17,39 +10,15 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `bd-huerta`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `login`
---
-
 CREATE TABLE `login` (
   `id` int(255) NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `login`
---
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `login`
---
 ALTER TABLE `login`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 COMMIT;
